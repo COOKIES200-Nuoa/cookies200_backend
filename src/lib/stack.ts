76af58093525e9f2e48ce8b2f0e9eec39daf1b83
Lactose Nham <128150779+lactosenham@users.noreply.github.com>
2024-06-15 12:04:16 +0700

    update stack.ts

--- src/lib/stack.ts
+++ src/lib/stack.ts
@@ -110,7 +110,7 @@ export class QuickSightIntegrationStack extends cdk.Stack {
       runtime: lambda.Runtime.NODEJS_20_X,
       handler: "index.onboarding",
       // so does Lambda return a Quicksight access token or create a new Quicksight account
-      code: lambda.Code.fromAsset("./lambda-code/onboarding"),
+      code: lambda.Code.fromAsset("src/lambda-code/onboarding"),
       role: lambdaRole, // Assign the role here
       environment: {
         REGION: 'ap-southeast-1',
@@ -126,6 +126,3 @@ export class QuickSightIntegrationStack extends cdk.Stack {
     });
   }
 }
-
-const app = new cdk.App();
-new QuickSightIntegrationStack(app, "QuickSightIntegrationStack");
