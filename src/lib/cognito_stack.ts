0d34caec763f28aa3e378ea3a17a757f287d28ff
s3938145 <103096849+s3938145@users.noreply.github.com>
2024-07-09 20:09:08 +0700

    Minor clean up in cognito_stack.ts

--- src/lib/cognito_stack.ts
+++ src/lib/cognito_stack.ts
@@ -4,11 +4,6 @@ import {
 } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Stack, StackProps, CfnOutput } from "aws-cdk-lib";
-// import * as cognito from 'aws-cdk-lib/aws-cognito';
-// import * as lambda from 'aws-cdk-lib/aws-lambda';
-// import * as iam from 'aws-cdk-lib/aws-iam';
-// import * as targets from 'aws-cdk-lib/aws-events-targets';
-// import * as cloudtrail from 'aws-cdk-lib/aws-cloudtrail';
 
 export class CognitoStack extends Stack {
   public readonly userPool: cognito.UserPool;
@@ -42,7 +37,6 @@ export class CognitoStack extends Stack {
         email: true,
       },
     });
-
 
     // Cognito Domain (Hosted UI)
     userPool.addDomain('EmbedDashboardDomain', {
@@ -133,146 +127,7 @@ export class CognitoStack extends Stack {
       })
     );
 
-    // // ========= Creating lambda function =========
-    // const lambdaRole = new iam.Role(this, "NuoaLambdaExecutionRole", {
-    //   assumedBy: new iam.CompositePrincipal( // Use CompositePrincipal to combine principals
-    //     new iam.ServicePrincipal("lambda.amazonaws.com"),
-    //     new iam.ServicePrincipal("quicksight.amazonaws.com")
-    //   ),
-    // });
-
-    // // Policies for creating Dashboard, Tenant Group, Tenant Role, and Role Mapping
-    // lambdaRole.addToPolicy(
-    //   new iam.PolicyStatement({
-    //     effect: iam.Effect.ALLOW,
-    //     actions: [
-    //       'quicksight:CreateNamespace',
-    //       'quicksight:CreateTemplate',
-    //       'quicksight:CreateAnalysis',
-    //       'quicksight:CreateDashboard',
-    //       'quicksight:PassDataSet',
-    //       'quicksight:UpdateAnalysisPermissions',
-    //       'quicksight:UpdateDashboardPermissions',
-    //       'quicksight:DescribeNamespace',
-    //       'quicksight:DescribeTemplate',
-    //       'quicksight:DescribeAnalysis',
-    //       'quicksight:DescribeDashboard',
-    //       'quicksight:RegisterUser',
-    //     ],
-    //     resources: ['*'],
-    //   })
-    // );
-
-    // lambdaRole.addToPolicy(
-    //   new iam.PolicyStatement({
-    //     effect: iam.Effect.ALLOW,
-    //     actions: [
-    //       'cognito-idp:CreateGroup',
-    //     ],
-    //     resources: ['*'],
-    //   })
-    // );
-
-    // lambdaRole.addToPolicy(
-    //   new iam.PolicyStatement({
-    //     effect: iam.Effect.ALLOW,
-    //     actions: [
-    //       'iam:CreateRole',
-    //       'iam:PutRolePolicy',
-    //       'iam:GetRole',
-    //       "iam:CreateServiceLinkedRole",
-    //       "iam:PutRolePolicy",
-    //       "iam:DeleteRole",
-    //       "iam:AttachRolePolicy",
-    //       "iam:DeleteRolePolicy",
-    //       "iam:ListRolePolicies",
-    //       "iam:ListAttachedRolePolicies",
-    //       "iam:DetachRolePolicy",
-    //     ],
-    //     resources: ["*"],
-    //   })
-    // );
-
-    // // Directory Services policies for creating Namespace
-    // lambdaRole.addToPolicy(
-    //   new iam.PolicyStatement({
-    //     effect: iam.Effect.ALLOW,
-    //     actions: [
-    //       "ds:CreateIdentityPoolDirectory",
-    //       "ds:DescribeDirectories",
-    //       "ds:AuthorizeApplication",
-    //     ],
-    //     resources: [`*`],
-    //   })
-    // );
-
-    // lambdaRole.addToPolicy(
-    //   new iam.PolicyStatement({
-    //     effect: iam.Effect.ALLOW,
-    //     actions: ["iam:PassRole"],
-    //     resources: [
-    //       `arn:aws:iam::${this.account}:role/*TenantRole`,
-    //       `${nuoaAuthRole.roleArn}`,
-    //     ],
-    //   })
-    // );
-
-    // lambdaRole.addToPolicy(
-    //   new iam.PolicyStatement({
-    //     effect: iam.Effect.ALLOW,
-    //     actions: [
-    //       "cognito-identity:SetIdentityPoolRoles",
-    //       "cognito-identity:GetIdentityPoolRoles",
-    //     ],
-    //     resources: [
-    //       `arn:aws:cognito-identity:${this.region}:${this.account}:identitypool/*`,
-    //     ],
-    //   })
-    // );
-
-    // const qsOnboardingFunction = new lambda.Function(this, 'QuickSightOnboardingLambda', {
-    //   runtime: lambda.Runtime.NODEJS_18_X,
-    //   handler: 'quicksightOnboarding.quicksightOnboarding',
-    //   code: lambda.Code.fromAsset('src/lambda-code/onboarding'),
-    //   role: lambdaRole,
-    //   environment: {
-    //     REGION: this.region,
-    //     AWS_ACC_ID: this.account,
-    //     QUICKSIGHT_ADMIN_ID: 'Cookies200',
-    //     USER_POOL_ID: userPool.userPoolId,
-    //     IDPOOL_ID: identityPool.ref,
-    //     USER_POOL_CLIENT_ID: userPoolClient.userPoolClientId,
-    //     AUTH_ROLE_ARN: nuoaAuthRole.roleArn,
-    //     DATASET: 'bc93b225-e6f7-4664-8331-99e66f5b7841', // Place holder dataset
-    //   },
-    //   timeout: cdk.Duration.minutes(1),
-    // });
-
-    // lambdaRole.addManagedPolicy(
-    //   iam.ManagedPolicy.fromAwsManagedPolicyName(
-    //     "service-role/AWSLambdaBasicExecutionRole"
-    //   )
-    // );
-
-    // const trail = new cloudtrail.Trail(this, "CloudTrail");
-
-    // const eventRule = cloudtrail.Trail.onEvent(this, "MyCloudWatchEvent", {
-    //   target: new targets.LambdaFunction(qsOnboardingFunction),
-    // });
-
-    // eventRule.addEventPattern({
-    //   account: [this.account],
-    //   source: ["aws.cognito-idp"],
-    //   detailType: ["AWS API Call via CloudTrail"],
-    //   detail: {
-    //     eventSource: ["cognito-idp.amazonaws.com"],
-    //     eventName: ["CreateGroup"],
-    //   },
-    // });
-
-    // Store the User Pool ID in the public property
     this.userPool = userPool;
-    // Store the User Pool client ID in the public property
     this.userPoolClientId = userPoolClient.userPoolClientId;
     this.nuoaAuthRoleARN = nuoaAuthRole.roleArn;
     this.identityPoolId = identityPool.ref;
