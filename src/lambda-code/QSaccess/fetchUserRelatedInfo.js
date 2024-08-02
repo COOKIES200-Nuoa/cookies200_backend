105e6da5f4571c7dec7d8ceb69b377ee9c0c162c
Linh-0v0 <s3864120@rmit.edu.vn>
2024-08-02 12:40:41 +0700

    Update analysis for author, dashboard for reader

--- src/lambda-code/QSaccess/fetchUserRelatedInfo.js
+++ src/lambda-code/QSaccess/fetchUserRelatedInfo.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const {
   QuickSightClient,
   GenerateEmbedUrlForRegisteredUserCommand,
+  DescribeUserCommand,
 } = require("@aws-sdk/client-quicksight");
 const { error } = require("console");
 
@@ -29,20 +30,58 @@ function getCognitoUserGroups(accessToken) {
   }
 }
 
+function getCognitoUsername(accessToken) {
+  try {
+    const decodedToken = jwt.decode(accessToken);
+    const usernames =
+      decodedToken["cognito:username"] || decodedToken["username"] || [];
+    if (!usernames || usernames.length === 0) {
+      throw new Error(`No group found in the access token ${error}`);
+    }
+    return usernames[0];
+  } catch (error) {
+    console.error("Failed to decode token:", error);
+    throw new Error(`Invalid token ${error}`);
+  }
+}
+
+async function getUserRole(accessToken) {
+  const userGroup = await getCognitoUserGroups(accessToken);
+  const username = await getCognitoUsername(accessToken);
+  const input = {
+    // DescribeUserRequest
+    UserName: username,
+    AwsAccountId: AWS_ACC_ID,
+    Namespace: userGroup,
+  };
+  try {
+    const command = new DescribeUserCommand(input);
+    const response = await quicksight.send(command);
+    return response.User.Role;
+  } catch (Error) {
+    console.error("Error: ", error);
+    throw new Error(error);
+  }
+}
+
 async function generateQuickSightURL(accessToken) {
   const userGroup = await getCognitoUserGroups(accessToken);
-  console.log("Usergroup: ", userGroup);
-
+  const userRole = await getUserRole(accessToken);
   const userArn = `arn:aws:quicksight:${AWS_REGION}:${AWS_ACC_ID}:user/${userGroup}/${userGroup}TenantRole/${userGroup}`;
-
   const dashboardId = `${userGroup}-dashboard`;
+  const analysisId = `${userGroup}-analysis`;
+
+  const experienceId = userRole == "READER" ? dashboardId : analysisId;
+  const initialPath =
+    userRole == "READER"
+      ? `/dashboards/${dashboardId}`
+      : `/analyses/${analysisId}`;
 
   const dashboardExperienceConfiguration = {
     Dashboard: {
-      InitialDashboardId: dashboardId,
+      InitialDashboardId: experienceId,
     },
   };
-
   const consoleExperienceConfiguration = {
     QuickSightConsole: {
       FeatureConfigurations: {
@@ -50,17 +89,17 @@ async function generateQuickSightURL(accessToken) {
           Enabled: true,
         },
       },
-      InitialPath: `/dashboards/${dashboardId}`,
+      InitialPath: initialPath,
     },
   };
-
   const params = {
+    // GenerateEmbedUrlForRegisteredUserRequest
     AwsAccountId: AWS_ACC_ID,
     UserArn: userArn,
     // SessionLifetimeInMinutes: 100,  // Adjust as necessary within the allowed range
     ExperienceConfiguration: consoleExperienceConfiguration,
   };
-
+  
   try {
     const command = new GenerateEmbedUrlForRegisteredUserCommand(params);
     const response = await quicksight.send(command);
